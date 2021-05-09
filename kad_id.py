1e572dd255493180f66a828721a6fe1dddd8f307
i.stupakevich <st.onlyemail@gmail.com>
2021-05-09 14:18:53 +0300

    add KadId class

---
+++ kad_id.py
@@ -0,0 +1,37 @@
+from typing import Final
+
+ID_LENGTH: int = 160
+
+class KadId:
+    @staticmethod
+    def distance(lhs_id, rhs_id):
+        return lhs_id^rhs_id
+    
+    @staticmethod
+    def compare_ref(lhs_id, rhs_id, ref) -> int:
+        lhs_dist = KadId.distance(lhs_id, ref)
+        rhs_dist = KadId.distance(rhs_id, ref)
+        if lhs_dist < rhs_dist:
+            return -1
+        if lhs_dist > rhs_dist:
+            return 1
+        return 0
+
+    @staticmethod
+    def compare_exp(lhs_id, rhs_id) -> int:
+        dist = KadId.distance(lhs_id, rhs_id)
+        bit = 0
+        while dist > 1:
+            bit += 1
+            dist >>= 1
+        return bit
+        
+    @staticmethod
+    def __validate_id(id: int):
+        assert id != None and id < 2**160, 'Invalid id'
+
+    @staticmethod
+    def __validate_index(index: int):
+        assert index != None and 0 <= index and index < ID_LENGTH, (
+                'Invalid index')  
+
